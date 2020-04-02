fbd189ceaff9934ff53bff24b1d2ffdf072d9db6
Martin Disch <Martin.Disch@digitecgalaxus.ch>
2020-04-02 16:34:47 +0200

    Introduce turn-taking

--- reactoe/src/index.js
+++ reactoe/src/index.js
@@ -15,13 +15,19 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      // TODO: if I recall correctly, that is bad practice (setState could
+      // be batched)
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -34,7 +40,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
