66be8e242d1cb5bdfe68d7d435a3d1f0076fab51
Martin Disch <Martin.Disch@digitecgalaxus.ch>
2020-04-07 14:54:49 +0200

    Prepare login mutation

--- graphql/client/src/index.tsx
+++ graphql/client/src/index.tsx
@@ -10,6 +10,16 @@ import injectStyles from "./styles";
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: "http://localhost:4000/",
+  headers: {
+    authorization: localStorage.getItem("token"),
+  },
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem("token"),
+    cartItems: [],
+  },
 });
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
