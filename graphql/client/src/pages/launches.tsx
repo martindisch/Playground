0490bbd41822c84059a5f26cc9a7ae2564209b24
Martin Disch <Martin.Disch@digitecgalaxus.ch>
2020-04-07 14:03:53 +0200

    Initialize client project

--- graphql/client/src/pages/launches.tsx
+++ graphql/client/src/pages/launches.tsx
@@ -1,11 +1,27 @@
-import React, { Fragment }  from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import { RouteComponentProps } from "@reach/router";
+
+import gql from "graphql-tag";
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    __typename
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
 
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
   return <div />;
-}
+};
 
 export default Launches;
-
