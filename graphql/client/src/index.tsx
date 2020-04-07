7088a87396a026fc4ed53224948cd9426cb4ee68
Martin Disch <Martin.Disch@digitecgalaxus.ch>
2020-04-07 15:29:55 +0200

    Add remaining local state management

--- graphql/client/src/index.tsx
+++ graphql/client/src/index.tsx
@@ -1,11 +1,14 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
+import Login from "./pages/login";
 import injectStyles from "./styles";
+import { resolvers, typeDefs } from "./resolvers";
+import gql from "graphql-tag";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -25,7 +28,20 @@ cache.writeData({
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link,
+  typeDefs,
+  resolvers,
 });
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 injectStyles();
 ReactDOM.render(
